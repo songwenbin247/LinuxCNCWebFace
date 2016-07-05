a81f6aa4500bd105c9aa998398c091e63c3ca520
MX-Master <mikhail@vydrenko.ru>
2016-07-06 01:41:29 +0600

    Second step with websockets in CMD panel
    Now all panels/scripts can create own websockets to any servers

--- js/command_panel.js
+++ js/command_panel.js
@@ -39,23 +39,23 @@ cmd.exec = function ( cmd_text )
     if ( log && log.add ) log.add("[CMD] " + cmd_text);
 
     if ( cmd_text.match(/^hal\s+/i) ) {
-        if ( hal_available ) {
-            websock.hal.sock.send( cmd_text.replace(/^hal\s+/i, "") + "\r\n" );
+        if ( cmd.halsock ) {
+            cmd.halsock.send( cmd_text.replace(/^hal\s+/i, "") + "\r\n" );
         } else {
-            if ( log && log.add ) log.add("[CMD] HAL isn't available","red");
+            if ( log && log.add ) log.add("[CMD] HAL socket isn't available","red");
         }
     } else if ( cmd_text.match(/^lcnc\s+/i) ) {
-        if ( lcnc_available ) {
-            websock.lcnc.sock.send( cmd_text.replace(/^lcnc\s+/i, "") + "\r\n" );
+        if ( cmd.lcncsock ) {
+            cmd.lcncsock.send( cmd_text.replace(/^lcnc\s+/i, "") + "\r\n" );
         } else {
-            if ( log && log.add ) log.add("[CMD] LCNC isn't available","red");
+            if ( log && log.add ) log.add("[CMD] LCNC socket isn't available","red");
         }
     }
 }
 
 cmd.on_cmd_send = function()
 {
-    var cmd_text = document.querySelector("#command_text").value.trim().replace(/;\s*/igm,"\r\n");
+    var cmd_text = document.querySelector("#command_text").value.trim();
     cmd.exec(cmd_text);
 }
 
@@ -74,6 +74,59 @@ cmd.js_init = function()
 
     document.querySelector("#command_text").addEventListener("keyup", cmd.on_cmd_input_keyup);
     document.querySelector("#command_send").addEventListener("click", cmd.on_cmd_send);
+    
+    // create a socket to talk with halrmt
+    cmd.halsock = websock.create(
+        "ws://"+parent.location.hostname+"/halrmt", // url
+        "telnet", // protocol
+        function(e) { // onopen callback
+            if ( log && log.add ) log.add("[CMD] [HAL] Socket is open","green");
+        },
+        function(e) { // onmessage callback
+            if ( log && log.add ) log.add("[CMD] [HAL] " + e.data);
+        },
+        function(e) { // onclose callback
+            if ( log && log.add ) log.add("[CMD] [HAL] Socket was closed ("+e.code+":"+e.message+")","red");
+            setTimeout(
+                function() {
+                    cmd.halsock = websock.create(
+                        cmd.halsock.url,
+                        cmd.halsock.protocol,
+                        cmd.halsock.onopen,
+                        cmd.halsock.onmessage,
+                        cmd.halsock.onclose
+                    );
+                },
+                2000
+            );
+        }
+    );
+    // create a socket to talk with linuxcncrsh
+    cmd.lcncsock = websock.create(
+        "ws://"+parent.location.hostname+"/linuxcncrsh", // url
+        "telnet", // protocol
+        function(e) { // onopen callback
+            if ( log && log.add ) log.add("[CMD] [LCNC] Socket is open","green");
+        },
+        function(e) { // onmessage callback
+            if ( log && log.add ) log.add("[CMD] [LCNC] " + e.data);
+        },
+        function(e) { // onclose callback
+            if ( log && log.add ) log.add("[CMD] [LCNC] Socket was closed ("+e.code+":"+e.message+")","red");
+            setTimeout(
+                function() {
+                    cmd.lcncsock = websock.create(
+                        cmd.lcncsock.url,
+                        cmd.lcncsock.protocol,
+                        cmd.lcncsock.onopen,
+                        cmd.lcncsock.onmessage,
+                        cmd.lcncsock.onclose
+                    );
+                },
+                2000
+            );
+        }
+    );
 }
 
 
--- js/websocket.js
+++ js/websocket.js
@@ -1,111 +1,20 @@
 // UTF8 without BOM
 
 var websock = {
-    hal: {
-        url: "ws://"+parent.location.hostname+"/halrmt",
-        proto: "telnet",
-        sock: false,
-        errCode: 0,
-        errText: "No errors"
-    },
-    lcnc: {
-        url: "ws://"+parent.location.hostname+"/linuxcncrsh",
-        proto: "telnet",
-        sock: false,
-        errCode: 0,
-        errText: "No errors"
-    },
-
-    checkInterval: 1000
 }
 
 
 
 
-websock.connection_test = function()
+websock.create = function( url, proto, on_open, on_message, on_close )
 {
-    if ( !parent.location.protocol.match("http") ) return;
+    var ws = new WebSocket(url, proto);
 
-    if ( websock.hal.sock && websock.hal.sock.readyState == 1 ) {
-        if ( log && log.add && !hal_available ) log.add("[WEBSOCK] HAL is here", "green");
-        hal_available = true;
-    } else {
-        if ( log && log.add && hal_available ) {
-            log.add("[WEBSOCK] HAL isn't available ("+websock.hal.errCode+":"+websock.hal.errText+")", "red");
-        }
-        hal_available = false;
-        websock.connect2hal();
-    }
+    ws.onopen       = typeof(on_open) == "function" ? on_open : ws.onopen;
+    ws.onmessage    = typeof(on_message) == "function" ? on_message : ws.onmessage;
+    ws.onclose      = typeof(on_close) == "function" ? on_close : ws.onclose;
 
-    if ( websock.lcnc.sock && websock.lcnc.sock.readyState == 1 ) {
-        if ( log && log.add && !lcnc_available ) log.add("[WEBSOCK] LCNC is here", "green");
-        lcnc_available = true;
-    } else {
-        if ( log && log.add && lcnc_available ) {
-            log.add("[WEBSOCK] LCNC isn't available ("+websock.lcnc.errCode+":"+websock.lcnc.errText+")", "red");
-        }
-        lcnc_available = false;
-        websock.connect2lcnc();
-    }
-}
-
-websock.connect2hal = function()
-{
-    if ( !parent.location.protocol.match("http") ) return;
-
-    var onclose = (websock.hal.sock && websock.hal.sock.onclose) ? 
-        websock.hal.sock.onclose :
-        function(event) {
-            websock.hal.errCode = event.code;
-            websock.hal.errText = event.reason;
-        };
-
-    var onerror = (websock.hal.sock && websock.hal.sock.onerror) ? 
-        websock.hal.sock.onerror :
-        function(event) {
-            websock.hal.errCode = 0;
-            websock.hal.errText = event.message;
-        };
-
-    var onmessage = (websock.hal.sock && websock.hal.sock.onmessage) ? 
-        websock.hal.sock.onmessage :
-        function(event) {
-            if ( log && log.add) log.add( "[HAL] " + event.data );
-        };
-
-    websock.hal.sock = new WebSocket(websock.hal.url, websock.hal.proto);
-    websock.hal.sock.onclose = onclose;
-    websock.hal.sock.onerror = onerror;
-    websock.hal.sock.onmessage = onmessage;
-}
-websock.connect2lcnc = function()
-{
-    if ( !parent.location.protocol.match("http") ) return;
-
-    var onclose = (websock.lcnc.sock && websock.lcnc.sock.onclose) ? 
-        websock.lcnc.sock.onclose :
-        function(event) {
-            websock.lcnc.errCode = event.code;
-            websock.lcnc.errText = event.reason;
-        };
-
-    var onerror = (websock.lcnc.sock && websock.lcnc.sock.onerror) ? 
-        websock.lcnc.sock.onerror :
-        function(event) {
-            websock.lcnc.errCode = 0;
-            websock.lcnc.errText = event.message;
-        };
-
-    var onmessage = (websock.lcnc.sock && websock.lcnc.sock.onmessage) ? 
-        websock.lcnc.sock.onmessage :
-        function(event) {
-            if ( log && log.add) log.add( "[LCNC] " + event.data );
-        };
-
-    websock.lcnc.sock = new WebSocket(websock.lcnc.url, websock.lcnc.proto);
-    websock.lcnc.sock.onclose = onclose;
-    websock.lcnc.sock.onerror = onerror;
-    websock.lcnc.sock.onmessage = onmessage;
+    return ws;
 }
 
 
@@ -115,9 +24,6 @@ websock.connect2lcnc = function()
 websock.js_init = function()
 {
     console.log("websock.js_init()");
-
-    websock.connect2hal();
-    websock.connection_test_timer = setInterval( websock.connection_test, websock.checkInterval );
 }
 
 
