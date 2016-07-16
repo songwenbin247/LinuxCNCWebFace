6b407cd3b5e86be601417785ae8b0e08ce7f4aed
MX-Master <mikhail@vydrenko.ru>
2016-07-16 14:20:13 +0600

    Added upgraded function loadto() to the main.js

--- js/main.js
+++ js/main.js
@@ -24,3 +24,46 @@ function n ( value )
     out = Number(out);
     return isNaN(out) || out == undefined ? 0 : out;
 }
+
+
+
+
+// load file into element
+function loadto ( file, replace, element, code )
+{
+    if ( !parent.location.protocol.match("http") ) return;
+    if ( loadto.busy ) return setTimeout( loadto, 200, file, element, code );
+    if ( typeof(element) == "string" ) element = document.querySelector(element);
+    if ( ! element ) return;
+
+    loadto.busy                     = true;
+    loadto.xhr                      = loadto.xhr ? loadto.xhr : new XMLHttpRequest();
+    loadto.xhr.open('GET', file, true);
+    loadto.xhr.replace              = replace;
+    loadto.xhr.destElement          = element;
+    loadto.xhr.evalCode             = code;
+    loadto.xhr.onreadystatechange   = loadto.xhr.onreadystatechange ? loadto.xhr.onreadystatechange :
+        function()
+        {
+            if ( this.readyState != 4 || !this.destElement ) return;
+
+            this.destElement.style.opacity = 0;
+            this.destElement.innerHTML = 
+                ( this.replace ? "" : this.destElement.innerHTML ) +
+                ( this.status == 200 ? this.responseText : this.status + ': ' + this.statusText );
+
+            setTimeout( 
+                function(e) {
+                    e.style.transition = "opacity 1s linear";
+                    e.style.opacity = 1;
+                }, 
+                50, 
+                this.destElement 
+            );
+
+            if ( this.evalCode ) setTimeout(this.evalCode, 1);
+
+            loadto.busy = false;
+        }
+    loadto.xhr.send();
+}
