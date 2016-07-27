6cd7e67c16cd3522026310c95d72255c5ea9ff52
MX-Master <mikhail@vydrenko.ru>
2016-07-27 20:26:36 +0600

    CTRL panel: added a few translation strings

--- js/control_panel.js
+++ js/control_panel.js
@@ -14,6 +14,14 @@ var ctrl =
 var lng_local_dic =
 [
     { en:"E-STOP", ru:"Е-СТОП" },
+    { en:"ON", ru:"ВКЛ" },
+    { en:"OFF", ru:"ВЫКЛ" },
+    { en:"Emergency STOP", ru:"Экстренный СТОП" },
+    { en:"Run opened program", ru:"Запустить открытую программу" },
+    { en:"Pause or resume program", ru:"Приостановить или возобновить программу" },
+    { en:"Abort program or MDI movements", ru:"Остановить программу или любые MDI движения" },
+    { en:"Machine OFF", ru:"Выключить станок" },
+    { en:"Machine ON", ru:"Включить станок" },
 ];
 
 // add local strings to translate to the global translate list
