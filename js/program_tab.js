451b93b94beb74d84c38d1a4450a593b6dd9f2c6
MX-Master <mikhail@vydrenko.ru>
2016-08-01 17:09:43 +0600

    Added click effect and handler functions for Program tab's buttons

--- js/program_tab.js
+++ js/program_tab.js
@@ -39,6 +39,60 @@ if ( window.localStorage ) prog.db = window.localStorage;
 
 
 
+prog.simpleClickAnimation = function ( id )
+{
+    document.querySelector("#"+id).style.opacity = "0";
+    setTimeout( 'document.querySelector("#'+id+'").style.opacity = "1";', 200 );
+}
+
+
+
+
+// some of the move buttons was clicked
+prog.btn_clicked = function ( event )
+{
+    var id;
+
+    if ( /_(file|program)$/.test(event.target.id) ) id = event.target.id;
+    else if ( /_(file|program)$/.test(event.target.parentElement.id) ) id = event.target.parentElement.id;
+    else return;
+
+    prog.simpleClickAnimation(id);
+
+    switch (id) {
+        case "open_file": 
+            break;
+        case "save_file": 
+            break;
+        case "upload_file": 
+            break;
+
+        case "play_program": 
+/*            if ( !ctrl.machine_on || ctrl.machine_estop || ctrl.program_status == "running" ) break;
+            if ( ctrl.program_status == "paused" ) {
+                ctrl.exec("set resume\r\n");
+            } else {
+                if ( typeof(prog) != "object" ) ctrl.exec("set mode auto\r\nset run\r\n");
+                else ctrl.exec("set mode auto\r\nset run " + prog.current_line + "\r\n");
+            }
+*/            break;
+        case "pause_program": 
+/*            if ( !ctrl.machine_on || ctrl.machine_estop || ctrl.program_status == "idle" ) break;
+            if ( ctrl.program_status == "running" ) ctrl.exec("set pause\r\n");
+            else ctrl.exec("set resume\r\n");
+*/            break;
+        case "step_program": 
+            break;
+        case "abort_program": 
+/*            if ( !ctrl.machine_on || ctrl.machine_estop ) break;
+            ctrl.exec("set abort\r\n");
+*/            break;
+    }
+}
+
+
+
+
 // do it when window is fully loaded
 prog.js_init = function()
 {
@@ -59,6 +113,8 @@ prog.js_init = function()
         function() {
             prog.tab = tabs.add("&#x2009;Program&#x2009;", prog.tab_content.innerHTML, 0, true);
             document.querySelector("body").removeChild(prog.tab_content);
+            // catch btns clicks
+            document.querySelector("#program_tools").addEventListener("click", prog.btn_clicked );
         }
     );
 }
