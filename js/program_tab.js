e605b099f47e9fbbfe1136fec17a2907b454f87b
MX-Master <mikhail@vydrenko.ru>
2016-08-07 22:14:32 +0600

    Buttons of Program tab are changed
    Texts replaced with chars

--- js/program_tab.js
+++ js/program_tab.js
@@ -18,13 +18,13 @@ var prog =
 var lng_local_dic =
 [
     { en:"Program", ru:"Программа" },
-    { en:"OPEN", ru:"ОТКРЫТЬ" },
-    { en:"SAVE", ru:"СОХРАНИТЬ" },
-    { en:"UPLOAD", ru:"ЗАГРУЗКА" },
-    { en:"RUN", ru:"ПУСК" },
-    { en:"PAUSE", ru:"ПАУЗА" },
-    { en:"STEP", ru:"ШАГ" },
-    { en:"ABORT", ru:"СТОП" },
+    { en:"Open file", ru:"Открыть файл" },
+    { en:"Save file", ru:"Сохранить файл" },
+    { en:"Upload files", ru:"Загрузка файлов" },
+    { en:"Run program", ru:"Пуск программы" },
+    { en:"Pause program", ru:"Пауза программы" },
+    { en:"Run only current line", ru:"Выполнить только текущую строку" },
+    { en:"Abort program", ru:"Стоп программы" },
     { en:"Current line", ru:"Текущая строка" },
     { en:"Current text position", ru:"Текущая позиция в тексте" },
 ];
