3169a1408619bb7cd4d497f970d2d26712401c0d
MX-Master <mikhail@vydrenko.ru>
2017-01-03 17:22:41 +0600

    1st step to async program file loading

--- web_files/module/program_tab/js/main.js
+++ web_files/module/program_tab/js/main.js
@@ -9,12 +9,18 @@ var prog =
 {
     db: {},
     
+    module_dir: "web_files/module/program_tab/",
+    
     current_line: 0,
     file: "",
     pages: 0,
+    last_page_id: 0,
     
     page_lines: 1000,
-    syntax_highlight: true
+    syntax_highlight: true,
+    
+    last_scroll_pos: 0,
+    max_scroll_offset: 2000
 };
 
 // have we a localStorage?
@@ -145,20 +151,20 @@ prog.load_file = function ( file_path )
 
     loadto(
         "web_files/module/program_tab/php/get_file_lines.php?file="+prog.file+"&start="+0+"&count="+prog.page_lines, "a", 
-        prog.add_page("", 0, prog.page_lines),
+        prog.add_page(0, "", 0, prog.page_lines),
         function() {
             log.add("[PROG] File `"+prog.file+"` loaded");
         }
     );
 }
 
-prog.add_page = function ( text, start_line, lines )
+prog.add_page = function ( id, text, start_line, lines )
 {
     var text_box = document.querySelector("#program_text");
     var page = document.createElement("DIV");
 
     page.className = "page";
-    page.setAttribute("data-page_id", prog.pages);
+    page.setAttribute("data-page_id", id);
     page.setAttribute("data-start_line", start_line);
     page.setAttribute("data-lines", lines);
 
@@ -169,6 +175,103 @@ prog.add_page = function ( text, start_line, lines )
     return page;
 }
 
+
+
+
+prog.on_scroll = function ( event )
+{
+    // when scroll bar almost at the top
+    if ( 
+        prog.last_scroll_pos >= prog.max_scroll_offset && 
+        this.scrollTop < prog.max_scroll_offset 
+    ) {
+        var top_page_id = prog.top_visible_page_id();
+        if ( top_page_id > 0 ) 
+        {
+            var prev_page_id = top_page_id - 1;
+            if ( prog.page_loaded(prev_page_id) ) 
+            {
+                if ( !prog.page_visible(prev_page_id) ) prog.show_page(prev_page_id);
+
+                var bottom_page_id = prog.bottom_visible_page_id();
+                if ( (bottom_page_id - top_page_id) > 1 ) // if we have more than 2 visible pages
+                { 
+                    if ( prog.page_visible(bottom_page_id) ) prog.hide_page(bottom_page_id);
+                }
+            }
+        }
+    } 
+    // when scroll bar almost at the bottom
+    else if ( 
+        prog.last_scroll_pos <= (this.scrollHeight - prog.max_scroll_offset) &&
+        this.scrollBottom > (this.scrollHeight - prog.max_scroll_offset) 
+    ) {
+        
+    }
+    
+    prog.last_scroll_pos = this.scrollTop;
+}
+
+
+
+
+prog.top_visible_page_id = function ( )
+{
+    var page = prog.text_box.querySelector("not(.page.hidden)");
+    return ( page ) ? page.getAttribute("data-page_id"); : 0;
+}
+prog.bottom_visible_page_id = function ( )
+{
+    var page = prog.text_box.querySelector(".page");
+    return ( page ) ? page.getAttribute("data-page_id"); : 0;
+}
+prog.page_loaded = function ( page_id )
+{
+    var page = prog.text_box.querySelector("[data-page_id="+page_id+"]");
+    return ( page ) ? true : false;
+}
+prog.page_visible = function ( page_id )
+{
+    var page = prog.text_box.querySelector("not(.page.hidden)[data-page_id="+page_id+"]");
+    return ( page ) ? true : false;
+}
+prog.page_hidden = function ( page_id )
+{
+    var page = prog.text_box.querySelector(".page.hidden[data-page_id="+page_id+"]");
+    return ( page ) ? true : false;
+}
+prog.load_page = function ( page_id )
+{
+    if ( page_id < 0 || (prog.last_page_id && page_id > prog.last_page_id) ) return;
+    
+    var page = document.createElement("DIV");
+
+    page.className = "page";
+    page.setAttribute("data-page_id", page_id);
+    page.setAttribute("data-start_line", page_id * prog.page_lines);
+    page.setAttribute("data-lines", prog.page_lines);
+
+    prog.text_box.appendChild(page);
+
+    loadto(
+        prog.module_dir+
+            "php/get_file_lines.php?file="+prog.file+
+            "&start="+(page_id * prog.page_lines)+
+            "&count="+prog.page_lines, 
+        "a", 
+        page
+    );
+}
+prog.show_page = function ( page_id )
+{
+    var page = prog.text_box.querySelector("[data-page_id="+page_id+"]");
+    if ( page ) page.classList.remove("hidden");
+}
+prog.hide_page = function ( page_id )
+{
+    var page = prog.text_box.querySelector("[data-page_id="+page_id+"]");
+    if ( page ) page.classList.add("hidden");
+}
 
 
 
@@ -194,8 +297,10 @@ prog.js_init = function()
             document.querySelector("body").removeChild(prog.tab_content);
             // catch btns clicks
             document.querySelector("#program_tools").addEventListener("click", prog.btn_clicked );
+            document.querySelector("#program_text").addEventListener("scroll", prog.on_scroll );
 //            document.querySelector("#program_text").addEventListener("click", prog.editor_update );
 //            document.querySelector("#program_text").addEventListener("keyup", prog.editor_update );
+            prog.text_box = document.querySelector("#program_text");
             lng.update();
         }
     );
