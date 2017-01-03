1f791846fda208ddc395da042332493c2978dda6
MX-Master <mikhail@vydrenko.ru>
2017-01-03 22:23:58 +0600

    Added code to output file lines count

--- web_files/module/program_tab/php/get_file_lines.php
+++ web_files/module/program_tab/php/get_file_lines.php
@@ -7,7 +7,7 @@ $file_path = isset($_GET['file']) ? get_full_path($_GET['file']) : "";
 if ( !is_file($file_path) ) exit('Wrong file path');
 
 // get file start line and lines count
-$file_start_line = isset($_GET['start']) ? (int) $_GET['start'] : 0;
+$file_start_line = isset($_GET['start']) ? (int) $_GET['start'] : -1;
 $file_lines_count = isset($_GET['count']) ? (int) $_GET['count'] : 0;
 if ( $file_lines_count <= 0 ) exit('Lines count <= 0');
 
@@ -71,29 +71,34 @@ if (
 
 
 
-// getting file start and end positions
 $pos_bytes = 4;
 
-$start = substr($_SESSION[$file_hash]['index'], $pos_bytes*$file_start_line, $pos_bytes);
-$end = substr($_SESSION[$file_hash]['index'], $pos_bytes*($file_start_line + $file_lines_count), $pos_bytes);
-session_write_close(); // unlock session files
-
-$start = ($start && strlen($start) == $pos_bytes) ? unpack("L",$start)[1] : false;
-$end = ($end && strlen($end) == $pos_bytes) ? unpack("L",$end)[1] : filesize($file_path);
-
-
-
-
-// output
-if ( is_int($start) && is_int($end) && $start < $end ) {
-    $file = fopen($file_path,'r');
-    fseek($file, $start, SEEK_SET);
-    $output = fread($file, $end - $start);
-    fclose($file);
-
-    print $output;
+if ( $file_start_line < 0 ) 
+{
+    // output file lines count
+    print strlen($_SESSION[$file_hash]['index']) / $pos_bytes - 1;
+    session_write_close(); // unlock session files
 }
+else
+{
+    // getting file start and end positions
+    $start = substr($_SESSION[$file_hash]['index'], $pos_bytes*$file_start_line, $pos_bytes);
+    $end = substr($_SESSION[$file_hash]['index'], $pos_bytes*($file_start_line + $file_lines_count), $pos_bytes);
+    session_write_close(); // unlock session files
 
+    $start = ($start && strlen($start) == $pos_bytes) ? unpack("L",$start)[1] : false;
+    $end = ($end && strlen($end) == $pos_bytes) ? unpack("L",$end)[1] : filesize($file_path);
+
+    // output file content
+    if ( is_int($start) && is_int($end) && $start < $end ) {
+        $file = fopen($file_path,'r');
+        fseek($file, $start, SEEK_SET);
+        $output = fread($file, $end - $start);
+        fclose($file);
+
+        print $output;
+    }
+}
 
 
 
